33355d13ac805210f1ba60ef98e4b9f71c454ebf
emonShimoul <emonshimoul@gmail.com>
2022-04-25 10:34:38 +0600

    add routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Friends></Friends>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/friends' element={<Friends></Friends>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
