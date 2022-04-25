991efa2bc4fbb2c9c8667ccdd9ee24b2dfa84b87
emonShimoul <emonshimoul@gmail.com>
2022-04-25 09:44:08 +0600

    install react router and some components

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>Page Not Found</h3>
+            <p>Don't as me about yhis page...</p>
+        </div>
+    );
+};
+
+export default NotFound;
